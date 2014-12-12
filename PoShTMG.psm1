770031af995536c7674b6d17f4b2d8112928e0c6
Nial Francis <nial@cognitionit.com.au>
2014-12-12 15:04:29 +1000

    indented tmg

--- PoShTMG.psm1
+++ PoShTMG.psm1
@@ -5,247 +5,246 @@
 ################################################################
 
 function New-TMGWebPublishingRule {
-Param( 
-[parameter(Mandatory=$true)] [string]$Name,
-[parameter(Mandatory=$true)] [string]$ServerHostName,
-[parameter(Mandatory=$true)] [string]$ServerIP,
-[parameter(Mandatory=$true)] [string]$WebListener,
-[parameter(Mandatory=$true)] [string]$PublicNames,
-[string]$SourceNetwork,
-[string]$InternalPathMapping,
-[string]$ExternalPathMapping,
-[bool]$SameAsInternalPath,
-[bool]$Action = 0,
-[bool]$TranslateLinks = 0,
-[int]$ServerAuthentication = 4
-)
+	Param( 
+		[parameter(Mandatory=$true)] [string]$Name,
+		[parameter(Mandatory=$true)] [string]$ServerHostName,
+		[parameter(Mandatory=$true)] [string]$ServerIP,
+		[parameter(Mandatory=$true)] [string]$WebListener,
+		[parameter(Mandatory=$true)] [string]$PublicNames,
+		[string]$SourceNetwork,
+		[string]$InternalPathMapping,
+		[string]$ExternalPathMapping,
+		[bool]$SameAsInternalPath,
+		[bool]$Action = 0,
+		[bool]$TranslateLinks = 0,
+		[int]$ServerAuthentication = 4
+	)
 
-if (-not($PolicyRules)) {
-$fpcroot = New-Object -ComObject fpc.root
-$tmgarray = $fpcroot.GetContainingArray()
-$global:PolicyRules = $tmgarray.ArrayPolicy.PolicyRules
-}
+	if (-not($PolicyRules)) {
+		$fpcroot = New-Object -ComObject fpc.root
+		$tmgarray = $fpcroot.GetContainingArray()
+		$global:PolicyRules = $tmgarray.ArrayPolicy.PolicyRules
+	}
 
-try {
-  $PolicyRules.Remove("$Name")
-} catch {}
+	try {
+	  $PolicyRules.Remove("$Name")
+	}
+	catch {	}
 
-$newrule = $PolicyRules.AddWebPublishingRule("$Name")
-$newrule.Action = $Action
-$newrule.WebPublishingProperties.WebSite = $ServerHostName
-$newrule.WebPublishingProperties.PublishedServer = $ServerIP
-if ($SourceNetwork) {$newrule.SourceSelectionIPs.Networks.RemoveAll(); $newrule.SourceSelectionIPs.Networks.Add("$SourceNetwork",0)}
-$newrule.WebPublishingProperties.SetWebListener($WebListener)
-$newrule.WebPublishingProperties.PublicNames.Add($PublicNames)
-$newrule.WebPublishingProperties.TranslateLinks = 0
-$newrule.WebPublishingProperties.CredentialsDelegationType = $ServerAuthentication
+	$newrule = $PolicyRules.AddWebPublishingRule("$Name")
+	$newrule.Action = $Action
+	$newrule.WebPublishingProperties.WebSite = $ServerHostName
+	$newrule.WebPublishingProperties.PublishedServer = $ServerIP
+	if ($SourceNetwork) {$newrule.SourceSelectionIPs.Networks.RemoveAll(); $newrule.SourceSelectionIPs.Networks.Add("$SourceNetwork",0)}
+	$newrule.WebPublishingProperties.SetWebListener($WebListener)
+	$newrule.WebPublishingProperties.PublicNames.Add($PublicNames)
+	$newrule.WebPublishingProperties.TranslateLinks = 0
+	$newrule.WebPublishingProperties.CredentialsDelegationType = $ServerAuthentication
 
-if ($SameAsInternalPath -eq 1) {$ExternalPathMapping = $InternalPathMapping}
-if ($InternalPathMapping) {$newrule.WebPublishingProperties.PathMappings.Add($InternalPathMapping,$SameAsInternalPath,$ExternalPathMapping)}
+	if ($SameAsInternalPath -eq 1) {$ExternalPathMapping = $InternalPathMapping}
+	if ($InternalPathMapping) {$newrule.WebPublishingProperties.PathMappings.Add($InternalPathMapping,$SameAsInternalPath,$ExternalPathMapping)}
 
-Write-Host "`nWhen you're finished, run Set-TMGRules to save your changes"
+	Write-Host "`nWhen you're finished, run Set-TMGRules to save your changes"
 }
 
 function New-TMGAccessRule {
-Param(
-[parameter(Mandatory=$true)] [string]$Name,
-[bool]$Action = 0,
-[int]$ProtocolSelectionMethod = 1,
-[string]$ProtocolName,
-[string]$AppliedComputerSet
-)
+	Param(
+		[parameter(Mandatory=$true)] [string]$Name,
+		[bool]$Action = 0,
+		[int]$ProtocolSelectionMethod = 1,
+		[string]$ProtocolName,
+		[string]$AppliedComputerSet
+	)
 
-if (-not($PolicyRules)) {
-$fpcroot = New-Object -ComObject fpc.root
-$tmgarray = $fpcroot.GetContainingArray()
-$global:PolicyRules = $tmgarray.ArrayPolicy.PolicyRules
-}
+	if (-not($PolicyRules)) {
+		$fpcroot = New-Object -ComObject fpc.root
+		$tmgarray = $fpcroot.GetContainingArray()
+		$global:PolicyRules = $tmgarray.ArrayPolicy.PolicyRules
+	}
 
-try {
-  $PolicyRules.Remove("$Name")
-} catch {}
+	try {
+		$PolicyRules.Remove("$Name")
+	}
+	catch { }
 
-$newrule = $PolicyRules.AddAccessRule("$Name")
-$newrule.Action = $Action
-$newrule.AccessProperties.ProtocolSelectionMethod = $ProtocolSelectionMethod
-$newrule.AccessProperties.SpecifiedProtocols.Add("$ProtocolName")
-$newrule.SourceSelectionIPs.ComputerSets.Add("$AppliedComputerSet")
+	$newrule = $PolicyRules.AddAccessRule("$Name")
+	$newrule.Action = $Action
+	$newrule.AccessProperties.ProtocolSelectionMethod = $ProtocolSelectionMethod
+	$newrule.AccessProperties.SpecifiedProtocols.Add("$ProtocolName")
+	$newrule.SourceSelectionIPs.ComputerSets.Add("$AppliedComputerSet")
 
-Write-Host "`nWhen you're finished, run Set-TMGRules to save your changes"
+	Write-Host "`nWhen you're finished, run Set-TMGRules to save your changes"
 }
 
 function New-TMGComputerSet {
-Param( 
-[parameter(Mandatory=$true)] [string]$Name
-)
+	Param( 
+		[parameter(Mandatory=$true)] [string]$Name
+	)
 
-if (-not($ComputerSet)) {
-$fpcroot = New-Object -ComObject fpc.root
-$tmgarray = $fpcroot.GetContainingArray()
-$global:ComputerSet = $tmgarray.RuleElements.ComputerSets
-}
+	if (-not($ComputerSet)) {
+		$fpcroot = New-Object -ComObject fpc.root
+		$tmgarray = $fpcroot.GetContainingArray()
+		$global:ComputerSet = $tmgarray.RuleElements.ComputerSets
+	}
 
-$newcs = $ComputerSet.Add($Name)
+	$newcs = $ComputerSet.Add($Name)
 
-Write-Host "`nWhen you're finished, run Set-TMGComputerSet to save your changes"
+	Write-Host "`nWhen you're finished, run Set-TMGComputerSet to save your changes"
 }
 
 function Add-TMGComputerToSet {
-Param( 
-[parameter(Mandatory=$true)] [string]$SetName,
-[parameter(Mandatory=$true)] [string]$ClientName,
-[parameter(Mandatory=$true)] [string]$ComputerIP
-)
+	Param( 
+		[parameter(Mandatory=$true)] [string]$SetName,
+		[parameter(Mandatory=$true)] [string]$ClientName,
+		[parameter(Mandatory=$true)] [string]$ComputerIP
+	)
 
-if (-not($ComputerSet)) {
-$fpcroot = New-Object -ComObject fpc.root
-$tmgarray = $fpcroot.GetContainingArray()
-$global:ComputerSet = $tmgarray.RuleElements.ComputerSets
-}
+	if (-not($ComputerSet)) {
+		$fpcroot = New-Object -ComObject fpc.root
+		$tmgarray = $fpcroot.GetContainingArray()
+		$global:ComputerSet = $tmgarray.RuleElements.ComputerSets
+	}
 
-$newcmp = $ComputerSet.item($SetName)
-$newcmp.Computers.Add($ClientName,$ComputerIP)
+	$newcmp = $ComputerSet.item($SetName)
+	$newcmp.Computers.Add($ClientName,$ComputerIP)
 
-Write-Host "`nWhen you're finished, run Set-TMGComputerSet to save your changes"
+	Write-Host "`nWhen you're finished, run Set-TMGComputerSet to save your changes"
 }
 
 function New-TMGProtocolDefinition {
-Param( 
-[parameter(Mandatory=$true)] [string]$Name
-)
+	Param( 
+		[parameter(Mandatory=$true)] [string]$Name
+	)
 
-if (-not($ComputerSet)) {
-$fpcroot = New-Object -ComObject fpc.root
-$tmgarray = $fpcroot.GetContainingArray()
-$global:Protocol = $tmgarray.RuleElements.ProtocolDefinitions
-}
+	if (-not($ComputerSet)) {
+		$fpcroot = New-Object -ComObject fpc.root
+		$tmgarray = $fpcroot.GetContainingArray()
+		$global:Protocol = $tmgarray.RuleElements.ProtocolDefinitions
+	}
 
-$newprot = $Protocol.Add($Name)
+	$newprot = $Protocol.Add($Name)
 
-Write-Host "`nWhen you're finished, run Set-TMGProtocols to save your changes"
+	Write-Host "`nWhen you're finished, run Set-TMGProtocols to save your changes"
 }
 
 function Add-TMGProtocolParameter {
-Param( 
-[parameter(Mandatory=$true)] [string]$Name,
-[parameter(Mandatory=$true)] [int]$LowPort,
-[parameter(Mandatory=$true)] [int]$HighPort,
-[parameter(Mandatory=$true)] [int]$Direction,
-[switch]$TCP,
-[switch]$UDP
-)
+	Param( 
+		[parameter(Mandatory=$true)] [string]$Name,
+		[parameter(Mandatory=$true)] [int]$LowPort,
+		[parameter(Mandatory=$true)] [int]$HighPort,
+		[parameter(Mandatory=$true)] [int]$Direction,
+		[switch]$TCP,
+		[switch]$UDP
+	)
 
-if (($TCP -eq $false) -and ($UDP -eq $false)) {throw "You must specify an IP protocol"}
+	if (($TCP -eq $false) -and ($UDP -eq $false)) {throw "You must specify an IP protocol"}
 
-if (-not($ComputerSet)) {
-$fpcroot = New-Object -ComObject fpc.root
-$tmgarray = $fpcroot.GetContainingArray()
-$global:Protocol = $tmgarray.RuleElements.ProtocolDefinitions
-}
+	if (-not($ComputerSet)) {
+		$fpcroot = New-Object -ComObject fpc.root
+		$tmgarray = $fpcroot.GetContainingArray()
+		$global:Protocol = $tmgarray.RuleElements.ProtocolDefinitions
+	}
 
-$newprot = $Protocol.Item($Name)
-if ($TCP) { $newprot.PrimaryConnections.AddTCP($Direction,$LowPort,$HighPort) }
-if ($UDP) { $newprot.PrimaryConnections.AddUDP($Direction,$LowPort,$HighPort) }
+	$newprot = $Protocol.Item($Name)
+	if ($TCP) { $newprot.PrimaryConnections.AddTCP($Direction,$LowPort,$HighPort) }
+	if ($UDP) { $newprot.PrimaryConnections.AddUDP($Direction,$LowPort,$HighPort) }
 
-Write-Host "`nWhen you're finished, run Set-TMGProtocols to save your changes"
+	Write-Host "`nWhen you're finished, run Set-TMGProtocols to save your changes"
 }
 
 
 function New-TMGWebListener {
-Param( 
-[parameter(Mandatory=$true)] [string]$Name,
-[string]$ListeningIP,
-[string]$CustomFormsDirectory,
-$RedirectHTTPAsHTTPS = 2,
-$SSLPort,
-$HTTPPort = 80,
-[int]$MaxConnections,
-[bool]$SSOEnabled = 0,
-[string]$SSODomainNames,
-[string]$CertThumbprint
-)
+	Param( 
+		[parameter(Mandatory=$true)] [string]$Name,
+		[string]$ListeningIP,
+		[string]$CustomFormsDirectory,
+		$RedirectHTTPAsHTTPS = 2,
+		$SSLPort,
+		$HTTPPort = 80,
+		[int]$MaxConnections,
+		[bool]$SSOEnabled = 0,
+		[string]$SSODomainNames,
+		[string]$CertThumbprint
+	)
 
-if (-not($WebListener)) {
-$fpcroot = New-Object -ComObject fpc.root
-$tmgarray = $fpcroot.GetContainingArray()
-$global:WebListener = $tmgarray.RuleElements.WebListeners
-}
+	if (-not($WebListener)) {
+	$fpcroot = New-Object -ComObject fpc.root
+	$tmgarray = $fpcroot.GetContainingArray()
+	$global:WebListener = $tmgarray.RuleElements.WebListeners
+	}
 
-try {
-  $WebListener.Remove("$Name")
-} catch {}
+	try {
+	  $WebListener.Remove("$Name")
+	}
+	catch { }
 
-$newrule = $WebListener.Add("$Name")
-$newrule.Properties.RedirectHTTPAsHTTPS = $RedirectHTTPAsHTTPS
-if ($SSLPort) {$newrule.Properties.SSLPort = $SSLPort}
-$newrule.Properties.TCPPort = $HTTPPort
-if ($MaxConnections -gt 0) {$newrule.Properties.NumberOfConnections = $MaxConnections}
-$newrule.Properties.AuthenticationSchemes.Add("FBA with AD",0)
-$newrule.Properties.SSOEnabled = $SSOEnabled
-if ($SSODomainNames) {$newrule.Properties.SSOEnabled = 1; $newrule.Properties.SSODomainNames.Add($SSODomainNames)}
-$newrule.Properties.FormsBasedAuthenticationProperties.CustomFormsDirectory = $CustomFormsDirectory
+	$newrule = $WebListener.Add("$Name")
+	$newrule.Properties.RedirectHTTPAsHTTPS = $RedirectHTTPAsHTTPS
+	if ($SSLPort) {$newrule.Properties.SSLPort = $SSLPort}
+	$newrule.Properties.TCPPort = $HTTPPort
+	if ($MaxConnections -gt 0) {$newrule.Properties.NumberOfConnections = $MaxConnections}
+	$newrule.Properties.AuthenticationSchemes.Add("FBA with AD",0)
+	$newrule.Properties.SSOEnabled = $SSOEnabled
+	if ($SSODomainNames) {$newrule.Properties.SSOEnabled = 1; $newrule.Properties.SSODomainNames.Add($SSODomainNames)}
+	$newrule.Properties.FormsBasedAuthenticationProperties.CustomFormsDirectory = $CustomFormsDirectory
 
-if ($ListeningIP) {
-  $newrule.IPsOnNetworks.Add("EXTERNAL",2,$ListeningIP)
-} else {
-  $newrule.IPsOnNetworks.Add("EXTERNAL",0,"")
-}
+	if ($ListeningIP) {
+	  $newrule.IPsOnNetworks.Add("EXTERNAL",2,$ListeningIP)
+	} else {
+	  $newrule.IPsOnNetworks.Add("EXTERNAL",0,"")
+	}
 
-if ($CertThumbprint) {
-$certhash = (gci cert:\LocalMachine\my\$CertThumbprint).getcerthash()
-$newrule.Properties.AppliedSSLCertificates.Add($certhash,"")
-}
+	if ($CertThumbprint) {
+		$certhash = (gci cert:\LocalMachine\my\$CertThumbprint).getcerthash()
+		$newrule.Properties.AppliedSSLCertificates.Add($certhash,"")
+	}
 
-
-
-Write-Host "`nWhen you're finished, run Set-TMGWebListener to save your changes"
+	Write-Host "`nWhen you're finished, run Set-TMGWebListener to save your changes"
 }
 
 function Set-TMGWebListener {
-if (-not($WebListener)) {throw "Nothing to save"}
-try { $WebListener.Save() }
-catch { throw $_.Exception.Message }
-write-host "Saving..."
-WaitForSync
+	if (-not($WebListener)) {throw "Nothing to save"}
+	try { $WebListener.Save() }
+	catch { throw $_.Exception.Message }
+	write-host "Saving..."
+	WaitForSync
 }
 
 function Set-TMGComputerSet {
-if (-not($ComputerSet)) {throw "Nothing to save"}
-try { $ComputerSet.Save() }
-catch { throw $_.Exception.Message }
-write-host "Saving..."
-WaitForSync
+	if (-not($ComputerSet)) {throw "Nothing to save"}
+	try { $ComputerSet.Save() }
+	catch { throw $_.Exception.Message }
+	write-host "Saving..."
+	WaitForSync
 }
 
 function Set-TMGRules {
-if (-not($PolicyRules)) {throw "Nothing to save"}
-try { $PolicyRules.Save() }
-catch { throw $_.Exception.Message }
-write-host "Saving..."
-WaitForSync
+	if (-not($PolicyRules)) {throw "Nothing to save"}
+	try { $PolicyRules.Save() }
+	catch { throw $_.Exception.Message }
+	write-host "Saving..."
+	WaitForSync
 }
 
 function Set-TMGProtocols {
-if (-not($Protocol)) {throw "Nothing to save"}
-try { $Protocol.Save() }
-catch { throw $_.Exception.Message }
-write-host "Saving..."
-WaitForSync
+	if (-not($Protocol)) {throw "Nothing to save"}
+	try { $Protocol.Save() }
+	catch { throw $_.Exception.Message }
+	write-host "Saving..."
+	WaitForSync
 }
 
 function WaitForSync {
 
-if (-not($TMGServer)) {
-$fpcroot = New-Object -ComObject fpc.root
-$global:TMGServer = $fpcroot.GetContainingServer()
+	if (-not($TMGServer)) {
+		$fpcroot = New-Object -ComObject fpc.root
+		$global:TMGServer = $fpcroot.GetContainingServer()
+	}
+
+	sleep 15
+	do {write-host "Waiting for sync...";sleep 10;$TMGServer.DistributionStatus.Refresh()}
+	while ($TMGServer.DistributionStatus.Status -ne 2)
+	write-host "Configuration synced!"
 }
-
-sleep 15
-do {write-host "Waiting for sync...";sleep 10;$TMGServer.DistributionStatus.Refresh()}
-while ($TMGServer.DistributionStatus.Status -ne 2)
-write-host "Configuration synced!"
-}
-
-
 
 export-modulemember *-*
